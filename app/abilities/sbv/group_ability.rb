695a1d84f35a6a02b0b5e0bae53129d64a44a876
Matthias Viehweger <viehweger@puzzle.ch>
2020-01-14 15:38:00 +0100

    Only show create-button for those authorized
    Fixes hitobito/hitobito_sbv#25

---
+++ app/abilities/sbv/group_ability.rb
@@ -0,0 +1,21 @@
+#  Copyright (c) 2019-2020, Schweizer Blasmusikverband. This file is part of
+#  hitobito_sbv and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sbv.
+
+module Sbv::GroupAbility
+  extend ActiveSupport::Concern
+
+  included do
+    on(Group) do
+      permission(:any).may(:'index_event/festivals').all
+      permission(:any).may(:query).all
+      permission(:festival_participation).may(:manage_festival_application).in_same_layer
+
+      permission(:group_full).may(:create_history_member).in_same_group
+      permission(:group_and_below_full).may(:create_history_member).in_same_group_or_below
+      permission(:layer_full).may(:create_history_member).in_same_layer
+      permission(:layer_and_below_full). may(:create_history_member).in_same_layer_or_below
+    end
+  end
+end
